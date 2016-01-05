20bc1c7e7749d0eab4fb9a9e5919af3565b5739c
CoyneServices <timbersplaysmc@gmail.com>
2016-01-04 20:50:50 -0500

    Revert "Revert "Here we go.""
    This reverts commit bff549834c6839a21059077ce6b4f73e95a8d1ec.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Authenticator 
+
+Welcome to the Authenticator Repository. Here you can find all the private authenticator information for staff.
---
+++ net/coyne/authserver/InvalidCredentialRequestException.java
@@ -0,0 +1,14 @@
+package net.coyne.authserver;
+
+/**
+ * Exception is thrown if invalid credentials/invalid credential format 
+ * are sent to the server. 
+ * @author Tim
+ *
+ */
+
+public class InvalidCredentialRequestException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+}
