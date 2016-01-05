bff549834c6839a21059077ce6b4f73e95a8d1ec
CoyneServices <timbersplaysmc@gmail.com>
2016-01-04 20:50:30 -0500

    Revert "Here we go."
    This reverts commit 78b29bd9c073192c64e6f976d721e3feeea656f5.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Authenticator 
-
-Welcome to the Authenticator Repository. Here you can find all the private authenticator information for staff.
--- net/coyne/authserver/ProtocolConstants.java
+++
@@ -1,8 +0,0 @@
-package net.coyne.authserver;
-
-public class ProtocolConstants {
-
-	/** Security Variable in Protocols **/
-	protected static final long PROTOCOL = -592305507;
-	
-}
